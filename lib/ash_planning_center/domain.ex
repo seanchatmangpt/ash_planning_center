d9d04fcbfddff667b902e7582dc6bc969e858419
Sean Chatman <136349053+seanchatmangpt@users.noreply.github.com>
2026-09-10 22:47:32 -0700

    fix: make missing remote people return nil explicitly

--- lib/ash_planning_center/domain.ex
+++ lib/ash_planning_center/domain.ex
@@ -7,7 +7,7 @@ defmodule AshPlanningCenter.Domain do
   resources do
     resource AshPlanningCenter.People.Person do
       define(:list_people, action: :read)
-      define(:get_person, action: :get, args: [:id])
+      define(:get_person, action: :get, args: [:id], not_found_error?: false)
     end
   end
 end
